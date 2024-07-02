d85ebffe1c3b043ed47b34b61fe2ede0a9eb9548
Nico Piovano <npiovano@sugit.com.ar>
2024-07-01 22:47:53 -0300

    first commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'class',
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    darkMode: 'class',
+    theme: {
+        extend: {
+            colors: {
+                "dark-moss-green": "#606c38ff",
+                "pakistan-green": "#283618ff",
+                "cornsilk": "#fefae0ff",
+                "earth-yellow": "#dda15eff",
+                "tigers-eye": "#bc6c25ff",
+            }
+        },
+    },
+    plugins: [],
 }
