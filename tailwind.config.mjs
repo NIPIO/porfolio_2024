ced8ea86fa42ff2d41f1939552cc2bcd04a306f2
NicoPiovano <npiovano@sugit.com.ar>
2026-01-08 15:03:23 -0300

    LightTheme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -11,6 +11,8 @@ export default {
                 "earth-yellow": "#dda15eff",
                 "tigers-eye": "#bc6c25ff",
                 "custom-green": "#22c55e",
+                "custom-green-dark": "#103900",
+                "charcoal-brown": "#34403A",
             }
         },
     },
